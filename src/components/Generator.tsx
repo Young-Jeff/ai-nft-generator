42337806749269d2a63e1f524cbbd91ae17edf03
feng <1416929275@qq.com>
2024-03-23 22:17:14 +0800

    feat：优化

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Button, Form, Input, Image, Spin, message, Steps } from "antd";
+import { Button, Form, Input, Image, Spin, message } from "antd";
 import axios from "axios";
 import { NFTStorage, File } from "nft.storage";
 import {
   useAccount,
   useWriteContract,
   useWaitForTransactionReceipt,
+  useBalance,
+  useReadContract,
 } from "wagmi";
 import aiNftAbi from "@/constants/ai-nft-abi.json";
 
@@ -15,73 +17,52 @@ const layout = {
 };
 
 export default function Generator() {
-  const setpsArr = [
-    {
-      title: "生成图片",
-      status: "wait",
-    },
-    {
-      title: "上传图片",
-      status: "wait",
-    },
-    {
-      title: "铸造完成",
-      status: "wait",
-    },
-  ];
   const [disabled, setDisabled] = useState(true);
   const [loading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
   const [image, setImage] = useState("");
-  const [steps, setSteps] = useState<any[]>([...setpsArr]);
+  const [statusTips, setStatusTips] = useState("");
   const account = useAccount();
+  const { data: balanceInfo } = useBalance({
+    address: account.address,
+    query: {
+      enabled: !!account.address,
+    },
+  });
   const {
     data: hash,
     writeContract: mint,
     isError: isMintError,
-    isPending: isMintLoading,
-    error: mintError,
   } = useWriteContract();
-  const { isSuccess: txSuccess, error: txError } = useWaitForTransactionReceipt(
-    {
-      hash,
-      query: {
-        enabled: !!hash,
-      },
+
+  const {
+    isSuccess: txSuccess,
+    isFetching,
+    error: txError,
+  } = useWaitForTransactionReceipt({
+    hash,
+    query: {
+      enabled: !!hash,
     },
-  );
-  useEffect(() => {
-    console.log(mintError);
-  }, [mintError]);
+  });
+
+  const { data: cost } = useReadContract({
+    abi: aiNftAbi,
+    address: "0x59f0e69b66baf0073d8a6145517b84ca98e329c2",
+    functionName: "getCost",
+  });
   useEffect(() => {
     if (isMintError || txError) {
-      setSteps((s) => {
-        const val = [...s];
-        val.splice(2, 10, {
-          title: "铸造失败",
-          status: "error",
-        });
-        return val;
-      });
-    } else if (isMintLoading) {
-      setSteps((s) => {
-        const val = [...s];
-        val.splice(2, 0, {
-          title: "铸造中",
-          status: "wait",
-        });
-        return val;
-      });
+      setStatusTips("铸造失败！！！");
+      setLoading(false);
+    } else if (isFetching) {
+      setStatusTips("铸造中...");
     } else if (txSuccess) {
-      setSteps((s) => {
-        const val = [...s];
-        val[2].status = "finish";
-        val[3].status = "finish";
-        return val;
-      });
+      setStatusTips("铸造成功！！！");
+      setLoading(false);
     }
-  }, [isMintLoading, isMintError, txError, txSuccess]);
+  }, [isMintError, txError, txSuccess, isFetching]);
   useEffect(() => {
     if (name && desc) {
       setDisabled(false);
@@ -90,6 +71,7 @@ export default function Generator() {
     }
   }, [name, desc]);
   const createImg = async () => {
+    setStatusTips("创建图片中...");
     const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
     const res = await axios({
       url: URL,
@@ -111,11 +93,10 @@ export default function Generator() {
     const base64data = Buffer.from(data).toString("base64");
     const img = `data:${type};base64,` + base64data;
     setImage(img);
-    steps[0].status = "finish";
-    setSteps(steps);
     return img;
   };
   const uploadImage = async (imgRaw: any) => {
+    setStatusTips("上传图片中...");
     const nftstorage = new NFTStorage({
       token: process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY as string,
     });
@@ -126,34 +107,36 @@ export default function Generator() {
       description: desc,
     });
     const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
-    steps[1].status = "finish";
-    setSteps(steps);
     return url;
   };
   const mintImage = async (url: string) => {
-    await mint({
+    setStatusTips("钱包签名中...");
+    mint({
       abi: aiNftAbi,
       address: "0x59f0e69b66baf0073d8a6145517b84ca98e329c2",
       functionName: "mintNft",
       args: [url],
+      value: cost as bigint,
     });
   };
   const submit = async () => {
-    setSteps(setpsArr);
     if (!account.isConnected) {
       message.warning("请先链接钱包");
+      return;
+    }
+    if (balanceInfo && cost && balanceInfo?.value < (cost as bigint)) {
+      message.warning("账户余额不足");
       return;
     }
     setLoading(true);
     const imgRaw = await createImg();
     const url = await uploadImage(imgRaw);
     await mintImage(url);
-    setLoading(false);
   };
   return (
     <div className="mt-[50px]">
-      <div className="h-[80px] w-[500px] mx-auto">
-        <Steps progressDot items={steps} />
+      <div className="h-[80px] w-[500px] mx-auto text-center text-[26px] font-[800] text-[#333]">
+        {statusTips}
       </div>
       <div className="flex justify-center items-center">
         <Form {...layout} size="large" className="w-[300px]">
@@ -186,7 +169,8 @@ export default function Generator() {
           {loading ? (
             <Spin size={"large"} />
           ) : (
-            image && (
+            image &&
+            txSuccess && (
               <Image
                 alt=""
                 width={"400px"}
